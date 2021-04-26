f2316e8e0c0873d25a842392652dd88f6c5e52b3
Ana Diaz <adiaz42@alumnos.uaq.mx>
2021-04-25 20:47:45 -0500

    Create e-commerce react app

---
+++ src/Components/Products/Products.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Products() {
+    return (
+        <div >
+            <div className="w-full h-64 bg-red-200"></div>
+
+        </div>
+    )
+}
