bf2ed9a94d9432ce35828b000d247d6625308a41
Ana Diaz <adiaz42@alumnos.uaq.mx>
2021-05-04 22:14:21 -0500

    Login created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Card from './Components/Products/Products'
+import Login from './Screens/Login'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
-
   return (
-    <div className="App">
-      <Card />
+    <div className="App w-full h-screen">
+      <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/" component={Login}/>
+        </Switch>
+    </Router>
 
     </div>
   )
---
+++ src/Screens/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className="w-full h-screen flex justify-center content-center items-center">
+                <div className=" w-80 h-80 flex flex-col justify-around items-center content-center">
+                    <div className="w-64 h-8 rounded-md border-2 pl-2 ">
+                        <input className="w-full h-full focus:outline-none" type="text" placeholder="usuario"></input>
+                    </div>
+                    <div className="w-64 h-8 rounded-md focus:outline-none border-2  pl-2 ">
+                        <input className="w-full h-full focus:outline-none"  type="password" placeholder="contraseña"></input>
+                    </div>
+                    <div className="w-64 h-8 border-2 border-transparent bg-blue-600 text-white  rounded-md">
+                        <button type="submit" className="w-full h-full text-sm focus:outline-none">Iniciar sesión</button>
+                    </div>
+                    <div className="w-64 h-8 border-2 border-transparent text-blue-600  rounded-md">
+                        <button type="submit" className="w-full h-full text-sm focus:outline-none">¿Aún no tienes cuenta?</button>
+                    </div>
+
+                </div>
+        </div>
+    )
+}
